1736833c6a11e6b34f7984652d864314f83ef35b
Arati Lama <la.arati24@ncmt.edu.np>
2026-09-01 08:10:16 +0545

    feat: folder structure changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-    "Thank you for visiting my portfolio!";
-}
+const menuButton = document.getElementById("menuBtn");
+const navigation = document.getElementById("navLinks");
+
+menuButton.addEventListener("click", function () {
+
+    navigation.classList.toggle("active");
+
+});
+
+
+const navigationLinks =
+    document.querySelectorAll(".nav-links a");
+
+
+navigationLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navigation.classList.remove("active");
+
+    });
+
+});
